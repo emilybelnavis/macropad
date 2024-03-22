54ff497729cd985ee2f3238e2b8256fcd21985af
Emily Belnavis <emily@emilybelnavis.ca>
2024-03-22 13:19:53 -0400

    macropad firmware now compiles and runs

--- readme.md
+++ readme.md
@@ -1,9 +1,45 @@
+---
+title: Homebrew Macropad Firmware
+defaultPlatform: mac
+defaultTool: cli
+---
 # macropad
 
 firmware for the homebrew macro pad
 
-# build and install instructions
+## build and install instructions
 
+### Install SDK Dependencies
+
+The instructions below assume that you are either on a macOS or Linux-based system. You will need to install CMake, the Arm GCC toolchain, and `libusb-1.0`
+
+#### macOS
+```bash
+brew install cmake libusb
+brew install --cask gcc-arm-embedded
+```
+
+#### Linux
+Use whatever package manager you want.
+```bash
+sudo apt install cmake build-essential libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib libusb-1.0.0
+```
+
+### Install the Pico C/C++ SDK
+
+Clone the Pico C/C++ SDK:
+```bash
+git clone https://github.com/raspberrypi/pico-sdk.git --branch master
+cd pico-sdk
+git submodule update --init
+```
+
+> Recommended to store this somewhere that is remembered easily either in `/usr/bin/lib/` or in `$HOME/Libraries/`
+
+### Install picotool
+If you are on macOS, you will already know that Finder is like a baby. Some macOS users have reported issues with trying to flash `.UF2` files
+
+###
 Build project by running the following:
 ```bash
 mkdir build
--- src/main.c
+++ src/main.c
@@ -14,32 +14,143 @@ enum {
     BLINK_SUSPENDED = 1500
 };
 
-static uint32_t blink_interval_ms = BLINK_UNMOUNTED;
+static uint32_t blinkIntervalMs = BLINK_UNMOUNTED;
 
-void led_blinking_task(void);
-void hid_task(void);
 
-void led_blinking_task(void) {
-    static uint32_t start_ms = 0;
-    static bool led_state = false;
+void ledBlinkTask(void) {
+    static uint32_t startMs = 0;
+    static bool ledState = false;
     
     // blink is disabled
-    if (!blink_interval_ms) return;
+    if (!blinkIntervalMs) return;
 
     // blink every [interval] ms
-    if (board_millis() - start_ms < blink_interval_ms) return;
-    start_ms += blink_interval_ms;
+    if (board_millis() - startMs < blinkIntervalMs) return;
+    startMs += blinkIntervalMs;
 
-    board_led_write(led_state);
+    board_led_write(ledState);
 
-    led_state = 1;
+    ledState = 1 - ledState;
 
 }
 
+// device callbacks
+void tusbMountCallback(void) {
+    blinkIntervalMs = BLINK_MOUNTED;
+}
+
+void tusbUnmountCallback(void) { 
+    blinkIntervalMs = BLINK_UNMOUNTED;
+}
+
+void tusbSuspendedCallback (void) {
+    blinkIntervalMs = BLINK_SUSPENDED;
+}
+
+void tusbResumeCallback (void) {
+    blinkIntervalMs = BLINK_MOUNTED;
+}
+
+// usb hid
+static void send_hid_report(uint8_t reportId, uint32_t btn) {
+    // check if hid is ready, if not ready then skip it
+    if (!tud_hid_ready() ) {
+        return;
+    }
+
+    switch(reportId) {
+        case REPORT_ID_KEYBOARD: {
+            // used to avoid sending multiple consecutive zero reports
+            static bool hasKeyboardKey = false;
+
+            if (btn) {
+                uint8_t keycode[6] = { 0 };
+                keycode[0] = HID_KEY_A; //sends "A" 
+
+                tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+                hasKeyboardKey = true;
+            } else {
+                if (hasKeyboardKey) {
+                    tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+                    hasKeyboardKey = false;
+                }
+            }
+        }
+        break;
+
+        default: break;
+    }
+}
+
+// send a HID report every 10ms
+void hid_task(void) {
+    const uint32_t intervalMs = 10;
+    static uint32_t startMs = 0;
+
+    if (board_millis() - startMs < intervalMs) {
+        return;
+    }
+
+    startMs += intervalMs;
+
+    uint32_t const btn = board_button_read();
+
+    if (tud_suspended() && btn) {
+        // wake up host if we are in a suspended mode
+        tud_remote_wakeup();
+    } else {
+        send_hid_report(REPORT_ID_KEYBOARD, btn);
+    }
+}
+
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len) {
+    (void) instance;
+    (void) len;
+
+    uint8_t nextReportId = report[0] + 1;
+    if (nextReportId < REPORT_ID_COUNT) {
+        send_hid_report(nextReportId, board_button_read());
+    }
+}
+
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t reportId, hid_report_type_t reportType, uint8_t* buffer, uint16_t reqlen) {
+    (void) instance;
+    (void) reportId;
+    (void) reportType;
+    (void) buffer;
+    (void) reqlen;
+
+    return 0;
+}
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t reportId, hid_report_type_t reportType, uint8_t const* buffer, uint16_t bufsize) {
+    (void) instance;
+
+    if(reportType == HID_REPORT_TYPE_OUTPUT) {
+        if (reportId == REPORT_ID_KEYBOARD) {
+            if (bufsize < 1) return;
+
+            uint8_t const keyboardLeds = buffer[0];
+
+            if(keyboardLeds & KEYBOARD_LED_CAPSLOCK) {
+                blinkIntervalMs = 0;
+                board_led_write(true);
+            } else {
+                board_led_write(false);
+                blinkIntervalMs = BLINK_MOUNTED;
+            }
+        }
+    }
+}
+
 int main(void) {
+    board_init();
+    tusb_init();
 
     while (1)
     {
-        led_blinking_task();
+        tud_task();
+        ledBlinkTask();
+        hid_task();
     }
 }
